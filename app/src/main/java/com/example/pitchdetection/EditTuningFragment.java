4efdf927033216847c7a6f5b2768a617e3cdaa35
ntina1 <ntinaliogka@gmail.com>
2023-08-05 13:51:03 +0300

    commit

---
+++ app/src/main/java/com/example/pitchdetection/EditTuningFragment.java
@@ -0,0 +1,58 @@
+package com.example.pitchdetection;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.List;
+
+
+public class EditTuningFragment extends Fragment {
+
+
+
+
+    private RecyclerView recyclerView;
+    private List<String> data;
+
+    public EditTuningFragment(List<String>  data) {
+        this.data=data;
+    }
+
+
+   
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view =inflater.inflate(R.layout.fragment_edit_tuning, container, false);
+        recyclerView = view.findViewById(R.id.editRecyclerViewer);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        MainActivity activity = (MainActivity) getActivity();
+
+        EditTuningRecyclerAdapter adapter = new EditTuningRecyclerAdapter(data, activity);
+
+        recyclerView.setAdapter(adapter);
+
+        Button close = view.findViewById(R.id.close);
+        close.setOnClickListener(view1 -> {
+
+            activity.onClickEditTView();
+        });
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/pitchdetection/EditTuningRecyclerAdapter.java
@@ -0,0 +1,94 @@
+package com.example.pitchdetection;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class EditTuningRecyclerAdapter extends RecyclerView.Adapter<EditTuningRecyclerAdapter.ViewHolder> {
+    static private List<String> items;
+    private static MainActivity activity;
+
+    public EditTuningRecyclerAdapter(List<String> items, MainActivity activity) {
+        EditTuningRecyclerAdapter.items = items.subList(11, items.size());
+        EditTuningRecyclerAdapter.activity = activity;
+    }
+
+    @Override
+    public EditTuningRecyclerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.edittuning_textview, parent, false);
+
+        return new EditTuningRecyclerAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(EditTuningRecyclerAdapter.ViewHolder holder, int position) {
+        String item = items.get(position);
+        holder.editTuningText.setText(item);
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView editTuningText;
+        private String clickedItem ;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            editTuningText = itemView.findViewById(R.id.editTuningText);
+
+            ImageButton editButton = itemView.findViewById(R.id.editButton);
+
+            editButton.setOnClickListener(v -> {
+                int position = getAdapterPosition();
+                    clickedItem = items.get(position);
+                    activity.onClickCustomTView(clickedItem,position);
+
+
+            });
+
+            ImageButton deleteButton = itemView.findViewById(R.id.deleteButton);
+            deleteButton.setOnClickListener(v ->{
+                AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+                builder.setTitle("Confirmation");
+                builder.setMessage("Are you sure you want to remove this item?");
+                builder.setPositiveButton("Yes", (dialog, which) -> {
+                    int position = getAdapterPosition();
+                    activity.removeTuning(activity ,position);
+
+                });
+                builder.setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss());
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            });
+
+
+
+        }
+
+
+
+
+
+    }
+
+
+}
